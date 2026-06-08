2f4050e81ea1df1ac3ec03c7b44a57e4d4a6191f
Jimmy Cai <jimmy@cai.im>
2026-06-08 11:47:58 +0200

    Export namespace constants

--- packages/language-server/src/analysis/default-namespaces.ts
+++ packages/language-server/src/analysis/default-namespaces.ts
@@ -1,12 +1,36 @@
+const LOCAL_NAMESPACE = "http://www.w3.org/2005/xquery-local-functions";
+const FN_NAMESPACE = "http://www.w3.org/2005/xpath-functions";
+const MATH_NAMESPACE = "http://www.w3.org/2005/xpath-functions/math";
+const MAP_NAMESPACE = "http://www.w3.org/2005/xpath-functions/map";
+const ARRAY_NAMESPACE = "http://www.w3.org/2005/xpath-functions/array";
+const XS_NAMESPACE = "http://www.w3.org/2001/XMLSchema";
+const XML_NAMESPACE = "http://www.w3.org/XML/1998/namespace";
+const JN_NAMESPACE = "http://jsoniq.org/functions";
+const JS_NAMESPACE = "http://jsoniq.org/types";
+const ERR_NAMESPACE = "http://www.w3.org/2005/xqt-errors";
+
+export {
+    LOCAL_NAMESPACE,
+    FN_NAMESPACE,
+    MATH_NAMESPACE,
+    MAP_NAMESPACE,
+    ARRAY_NAMESPACE,
+    XS_NAMESPACE,
+    XML_NAMESPACE,
+    JN_NAMESPACE,
+    JS_NAMESPACE,
+    ERR_NAMESPACE,
+};
+
 export const defaultNamespaces: Map<string, string> = new Map([
-    ["local", "http://www.w3.org/2005/xquery-local-functions"],
-    ["fn", "http://www.w3.org/2005/xpath-functions"],
-    ["math", "http://www.w3.org/2005/xpath-functions/math"],
-    ["map", "http://www.w3.org/2005/xpath-functions/map"],
-    ["array", "http://www.w3.org/2005/xpath-functions/array"],
-    ["xs", "http://www.w3.org/2001/XMLSchema"],
-    ["xml", "http://www.w3.org/XML/1998/namespace"],
-    ["jn", "http://jsoniq.org/functions"],
-    ["js", "http://jsoniq.org/types"],
-    ["err", "http://www.w3.org/2005/xqt-errors"],
+    ["local", LOCAL_NAMESPACE],
+    ["fn", FN_NAMESPACE],
+    ["math", MATH_NAMESPACE],
+    ["map", MAP_NAMESPACE],
+    ["array", ARRAY_NAMESPACE],
+    ["xs", XS_NAMESPACE],
+    ["xml", XML_NAMESPACE],
+    ["jn", JN_NAMESPACE],
+    ["js", JS_NAMESPACE],
+    ["err", ERR_NAMESPACE],
 ]);
