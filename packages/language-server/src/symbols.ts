9663bd494bb3a78710fcf2fb6f9496928fde1b6c
Jimmy Cai <jimmy@cai.im>
2026-06-13 20:48:47 +0200

    refactor: remove analytics/symbols.ts and move DocumentSymbolsBuilder to symbols.ts

--- packages/language-server/src/symbols.ts
+++ packages/language-server/src/symbols.ts
@@ -1,11 +1,74 @@
-import { DocumentSymbol } from "vscode-languageserver";
+import { DocumentSymbol, SymbolKind } from "vscode-languageserver";
 import { TextDocument } from "vscode-languageserver-textdocument";
 
-import { DocumentSymbolsBuilder } from "./analysis/symbols.js";
+import { AstNode, DeclarationNode } from "./analysis/ast.js";
+import { definitionNameToString } from "./analysis/definitions.js";
+import { QNameToString } from "./analysis/names.js";
+import { getAnalysis } from "./analysis/service.js";
+import { AstVisitor } from "./analysis/visitor.js";
 
 /**
  * Collects DocumentSymbols from the given TextDocument.
  */
 export async function collectDocumentSymbols(document: TextDocument): Promise<DocumentSymbol[]> {
     return new DocumentSymbolsBuilder(document).build();
+}
+
+export class DocumentSymbolsBuilder extends AstVisitor<DocumentSymbol[]> {
+    private readonly document: TextDocument;
+
+    public constructor(document: TextDocument) {
+        super();
+        this.document = document;
+    }
+
+    public build(): DocumentSymbol[] {
+        const analysis = getAnalysis(this.document);
+        return this.visit(analysis.ast);
+    }
+
+    protected override defaultVisit(node: AstNode): DocumentSymbol[] {
+        return this.visitChildren(node).flat();
+    }
+
+    protected override visitDeclaration(node: DeclarationNode): DocumentSymbol[] {
+        const declaration = node.declaration;
+        const name =
+            declaration.kind === "function"
+                ? QNameToString(declaration.name.qname, false)
+                : definitionNameToString(declaration, false);
+
+        if (name.trim() === "" || name === "$") {
+            return this.visitChildren(node).flat();
+        }
+
+        let kind: SymbolKind;
+
+        switch (declaration.kind) {
+            case "namespace":
+                kind = SymbolKind.Namespace;
+                break;
+            case "type":
+                kind = SymbolKind.Struct;
+                break;
+            case "function":
+                kind = SymbolKind.Function;
+                break;
+            default: // all variable kinds, parameters, etc.
+                kind = SymbolKind.Variable;
+                break;
+        }
+
+        const childSymbols = this.visitChildren(node).flat();
+
+        const symbol: DocumentSymbol = {
+            name,
+            kind,
+            range: declaration.range,
+            selectionRange: declaration.selectionRange,
+            children: childSymbols,
+        };
+
+        return [symbol];
+    }
 }
