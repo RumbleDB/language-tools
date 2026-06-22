56dcb12569f6de10276d38abf73c7e30ce659bbb
Jimmy <jimmy@cai.im>
2026-06-23 00:01:06 +0200

    feat: enhance TypeDefinition structure to support object and array types (#36)

--- packages/rumble-lsp-wrapper/src/main/java/org/jsoniq/lsp/wrapper/types/TypeDefinition.java
+++ packages/rumble-lsp-wrapper/src/main/java/org/jsoniq/lsp/wrapper/types/TypeDefinition.java
@@ -1,17 +1,55 @@
 package org.jsoniq.lsp.wrapper.types;
 
 import org.rumbledb.types.ItemType;
+import org.rumbledb.types.FieldDescriptor;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public record TypeDefinition(
-        ResolvedQName name) {
+        String kind,
+        ResolvedQName name,
+        Map<String, TypeDefinition> fields) {
 
     public static TypeDefinition fromItemType(ItemType itemType) {
+        if (itemType.isObjectItemType()) {
+            Map<String, TypeDefinition> fields = new LinkedHashMap<>();
+            for (FieldDescriptor fieldDescriptor : itemType.getObjectContentFacet()) {
+                fields.put(
+                        fieldDescriptor.getName(),
+                        TypeDefinition.fromItemType(fieldDescriptor.getType()));
+            }
+            return new TypeDefinition(
+                    "object",
+                    itemType.hasName() ? ResolvedQName.fromName(itemType.getName()) : null,
+                    fields);
+        }
+
+        if (itemType.isArrayItemType()) {
+            return new TypeDefinition(
+                    "array",
+                    itemType.hasName() ? ResolvedQName.fromName(itemType.getName()) : null,
+                    null);
+        }
+
         return new TypeDefinition(
-                itemType.hasName() ? ResolvedQName.fromName(itemType.getName()) : null);
+                "named",
+                itemType.hasName() ? ResolvedQName.fromName(itemType.getName()) : null,
+                null);
     }
 
     @Override
     public String toString() {
+        if (this.kind.equals("object")) {
+            String fieldsString = this.fields == null
+                    ? ""
+                    : this.fields.entrySet().stream()
+                            .map(field -> field.getKey() + ": " + field.getValue())
+                            .reduce((left, right) -> left + ", " + right)
+                            .orElse("");
+            return "{ " + fieldsString + " }";
+        }
+
         return this.name() != null ? this.name().toString() : "anonymous type";
     }
 }
