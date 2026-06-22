b3d7cc9ecc0a3d3b5fbf6881377229e3776be2e6
Jimmy Cai <jimmy@cai.im>
2026-06-22 13:38:46 +0200

    feat: add builtin types definition and loading functionality

---
+++ packages/language-server/src/assets/builtin-types.ts
@@ -0,0 +1,45 @@
+import { QNameToString, type QName } from "server/analysis/names.js";
+import { type WrapperResolvedQName, toResolvedQName } from "server/wrapper/names.js";
+
+import { loadJsonAsset } from "./loader.js";
+
+export interface BuiltinTypeDefinition {
+    name: QName;
+    type: string;
+    baseType: QName | undefined;
+    isBuiltin: true;
+}
+
+function findBuiltinTypeDefinition(
+    map: Map<string, BuiltinTypeDefinition>,
+    name: QName,
+): BuiltinTypeDefinition | undefined {
+    return map.get(QNameToString(name, true));
+}
+
+const map = new Map<string, BuiltinTypeDefinition>();
+const catalog =
+    loadJsonAsset<
+        Array<{
+            name: WrapperResolvedQName;
+            type: string;
+            baseType: WrapperResolvedQName | null;
+        }>
+    >("builtin-types.json") || [];
+
+for (const builtinType of catalog) {
+    const name = toResolvedQName(builtinType.name);
+    const baseType = builtinType.baseType ? toResolvedQName(builtinType.baseType) : undefined;
+
+    map.set(QNameToString(name, true), {
+        name,
+        type: builtinType.type,
+        baseType,
+        isBuiltin: true,
+    });
+}
+
+export const builtinTypes = {
+    all: [...map.values()],
+    find: (name: QName) => findBuiltinTypeDefinition(map, name),
+};
