542d291514a063d396930209ba9ce96e18faf06b
Jimmy Cai <jimmy@cai.im>
2026-08-04 19:29:42 +0200

    Remove unused import

--- packages/rumble-lsp-wrapper/src/test/java/org/jsoniq/lsp/wrapper/RunQueryTest.java
+++ packages/rumble-lsp-wrapper/src/test/java/org/jsoniq/lsp/wrapper/RunQueryTest.java
@@ -1,7 +1,6 @@
 package org.jsoniq.lsp.wrapper;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
