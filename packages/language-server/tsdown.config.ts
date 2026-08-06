9e42a78308cf48c8a5fb32830f8aab083168dd98
Jimmy Cai <jimmy@cai.im>
2026-08-06 12:04:52 +0200

    fix: add 'pidusage' to alwaysBundle dependencies in tsdown config

--- packages/language-server/tsdown.config.ts
+++ packages/language-server/tsdown.config.ts
@@ -42,6 +42,7 @@ export default defineConfig([
                 "vscode-languageserver",
                 "vscode-languageserver/node",
                 "vscode-languageserver-textdocument",
+                "pidusage",
             ],
         },
     },
