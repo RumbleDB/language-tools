441aba3fb45529ccdb0998a38a2bf842d56d30ad
Jimmy Cai <jimmy@cai.im>
2026-08-11 19:15:49 +0200

    refactor: prune empty module graph entries

--- packages/language-server/src/workspace/module-graph.ts
+++ packages/language-server/src/workspace/module-graph.ts
@@ -10,7 +10,9 @@ export class ModuleGraph {
         dependencies: ReadonlySet<DocumentUri>,
     ): void {
         for (const previous of this.dependencies.get(importer) ?? []) {
-            this.dependents.get(previous)?.delete(importer);
+            const dependents = this.dependents.get(previous);
+            dependents?.delete(importer);
+            if (dependents?.size === 0) this.dependents.delete(previous);
         }
 
         this.dependencies.set(importer, new Set(dependencies));
